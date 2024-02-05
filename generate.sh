2bfb50c23eb2a8da9c595a9d94b237d4861d856e
kukv <rio.by.kukv@gmail.com>
2024-02-06 01:04:36 +0900

    ログメッセージを追加

--- generate.sh
+++ generate.sh
@@ -3,14 +3,28 @@
 set -euo pipefail
 cd "$(dirname "${0}")"
 
+echo "########################################################################################################"
+echo "########################################################################################################"
+echo "###                                                                                                  ###"
+echo "### Generate cloud-init setting files.                                                               ###"
+echo "###                                                                                                  ###"
+echo "########################################################################################################"
+echo "########################################################################################################"
+
 while read -r server; do
     export MACHINE_NAME="${server}"
+    echo "===================================================================================================="
+    echo "= generating gateway settings a ${server}."
+    echo "===================================================================================================="
     docker compose -f network-generator.yml up --remove-orphans
     docker compose -f user-data-generator.yml up --remove-orphans
 done < ../gateway-list
 
 while read -r server; do
     export MACHINE_NAME="${server}"
+    echo "===================================================================================================="
+    echo "= generating cluster settings a ${server}."
+    echo "===================================================================================================="
     docker compose -f user-data-generator.yml up --remove-orphans
 done < ../cluster-list
 
