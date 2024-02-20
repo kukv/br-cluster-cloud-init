b7bc168fb48d1bf39b61025755aa9c47d9dacca2
kukv <rio.by.kukv@gmail.com>
2024-02-20 13:46:24 +0900

    外部サービス用のサーバーのcloud-init定義を追加

--- generate.sh
+++ generate.sh
@@ -23,6 +23,14 @@ done < ../gateway-list
 while read -r server; do
     export MACHINE_NAME="${server}"
     echo "===================================================================================================="
+    echo "= generating external service settings a ${server}."
+    echo "===================================================================================================="
+    docker compose -f user-data-generator.yml up --remove-orphans
+done < ../external-service-list
+
+while read -r server; do
+    export MACHINE_NAME="${server}"
+    echo "===================================================================================================="
     echo "= generating cluster settings a ${server}."
     echo "===================================================================================================="
     docker compose -f user-data-generator.yml up --remove-orphans
